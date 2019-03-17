83707b2465416ee7b878ca8c0569e48caee800b2
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 17:42:28 -0400

    extended yarn install timeout on heroku

--- herokuInit.sh
+++ herokuInit.sh
@@ -3,7 +3,7 @@ if [ ! -f './src/config/private.js' ]; then
 export NODE_ENV=development;
 export YARN_PRODUCTION=false;
   echo "yarn install --non-interactive --ignore-engines --production=false";
-  yarn install --non-interactive --ignore-engines --production=false;
+  yarn install --non-interactive --ignore-engines --production=false --network-timeout 48000;
 export NODE_ENV=production;
 export YARN_PRODUCTION=true;
 export HEROKU_URL=$(heroku info -s -a dgmd-e60 | grep web_url | cut -d= -f2)
