3f9020066d2be880566b29c325e101ce253c600c
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 17:46:50 -0400

    extended OTHER yarn install timeout for heroku :(

--- herokuDevDeps.sh
+++ herokuDevDeps.sh
@@ -4,6 +4,6 @@ echo "installing dev dependencies"
 
 export NODE_ENV=development;
 export NPM_CONFIG_PRODUCTION=false;
-  yarn install --non-interactive --ignore-engines --production=false;
+  yarn install --non-interactive --ignore-engines --production=false --network-timeout 48000;
 export NODE_ENV=production;
 export NPM_CONFIG_PRODUCTION=true;
