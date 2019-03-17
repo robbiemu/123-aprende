ff42cb8268575394d3d35da88000a8b67b02974e
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 15:58:57 -0400

    heroku/gitlab integration changes

--- herokuInit.sh
+++ herokuInit.sh
@@ -4,6 +4,7 @@ export NPM_CONFIG_PRODUCTION=false;
   yarn clean;
 export NODE_ENV=production;
 export NPM_CONFIG_PRODUCTION=true;
+export HEROKU_URL=$(heroku info -s -a dgmd-e60 | grep web_url | cut -d= -f2)
   yarn build-web;
 fi
 
