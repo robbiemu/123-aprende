9bb0fcad053764c719e361d8f4a9ddafda1ff05a
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 18:16:51 -0400

    modified heroku env to keep devDeps

--- herokuInit.sh
+++ herokuInit.sh
@@ -1,13 +1,6 @@
 if [ ! -f './src/config/private.js' ]; then
-    echo $PRIVATE_JS > ./src/config/private.js
-export NODE_ENV=development;
-export YARN_PRODUCTION=false;
-  echo "yarn install --non-interactive --ignore-engines --production=false --network-timeout 48000";
-  yarn install --non-interactive --ignore-engines --production=false --network-timeout 48000;
-export NODE_ENV=production;
-export YARN_PRODUCTION=true;
-export HEROKU_URL=$(heroku info -s -a dgmd-e60 | grep web_url | cut -d= -f2)
-  yarn build-web;
+  echo $PRIVATE_JS > ./src/config/private.js
 fi
 
+yarn build-web;
 yarn serve-web;
