b685b953bb3f579ee11643ee106692830b5be5ee
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 17:24:26 -0400

    changed production env for heroku

--- herokuInit.sh
+++ herokuInit.sh
@@ -1,11 +1,11 @@
 if [ ! -f './src/config/private.js' ]; then
     echo $PRIVATE_JS > ./src/config/private.js
 export NODE_ENV=development;
-export NPM_CONFIG_PRODUCTION=false;
+export YARN_PRODUCTION=false;
   echo "yarn install --non-interactive --ignore-engines --production=false";
   yarn install --non-interactive --ignore-engines --production=false;
 export NODE_ENV=production;
-export NPM_CONFIG_PRODUCTION=true;
+export YARN_PRODUCTION=true;
 export HEROKU_URL=$(heroku info -s -a dgmd-e60 | grep web_url | cut -d= -f2)
   yarn build-web;
 fi
