82cb90c5b160d3211e6f637e740ec7c1aa46b45d
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 16:46:17 -0400

    change to heroku build and removed early gitlab ci tests

--- herokuInit.sh
+++ herokuInit.sh
@@ -1,7 +1,7 @@
 if [ ! -f './server/index.js' ]; then
 export NODE_ENV=development;
 export NPM_CONFIG_PRODUCTION=false;
-  yarn install;
+  yarn install --non-interactive --ignore-engines --production=false;
 export NODE_ENV=production;
 export NPM_CONFIG_PRODUCTION=true;
 export HEROKU_URL=$(heroku info -s -a dgmd-e60 | grep web_url | cut -d= -f2)
