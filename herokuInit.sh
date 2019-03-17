65083bb7651e58e17c5c86f7e1917e5bc7327bac
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 16:54:16 -0400

    hb

--- herokuInit.sh
+++ herokuInit.sh
@@ -2,6 +2,7 @@ if [ ! -f './server/index.js' ]; then
     echo $PRIVATE_JS > ./src/config/private.js
 export NODE_ENV=development;
 export NPM_CONFIG_PRODUCTION=false;
+  echo "yarn install --non-interactive --ignore-engines --production=false";
   yarn install --non-interactive --ignore-engines --production=false;
 export NODE_ENV=production;
 export NPM_CONFIG_PRODUCTION=true;
