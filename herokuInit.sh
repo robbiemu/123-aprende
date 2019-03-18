b05c36d9ac2a18036d5a0d0efe838b84ccb48602
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-18 00:52:58 -0400

    ..

--- herokuInit.sh
+++ herokuInit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for filename in build/static/js/*; do
-    sed -i "s?__CALLBACK__?$HEROKU_URL:$PORT\/callback?" $filename
+    sed -i "s?__CALLBACK__?$HEROKU_URL\/callback?" $filename
 done
 
 yarn serve-web
