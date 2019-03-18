e0752275a11056f87c1fe4a88176f28724328f00
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 23:15:16 -0400

    heroku - auth0 integration

---
+++ herokuAuth0Update.js
@@ -0,0 +1,30 @@
+var ManagementClient = require('auth0').ManagementClient;
+var management = new ManagementClient({
+  domain: 'third-party-demo.auth0.com',
+  clientId: `${process.env.AUTH0_CLIENT_ID}`,
+  clientSecret: `${process.env.AUTH0_CLIENT_SECRET}`,
+  scope: 'read:client_keys update:client_keys'
+});
+
+management.getClient({ client_id: process.env.AUTH0_CLIENT_ID }, function (err, client) {
+  if (err) {
+    return console.log(err)
+  }
+
+  callbacks = client.callbacks
+  callbacks.map(uri => {
+    if(new Regexp(process.env.HEROKU_HOSTNAME).text(uri)) {
+      uri = `${process.env.HEROKU_URL}:${process.env.PORT}`
+    }
+    return uri
+  })
+
+  var data = { callbacks };
+  var params = { client_id: process.env.AUTH0_CLIENT_ID };
+
+  management.client.update(params, data, function (err, client) {
+    if (err) {
+      return console.log(err)
+    }
+  });
+});
--- herokuInit.sh
+++ herokuInit.sh
@@ -4,4 +4,6 @@ for filename in build/static/js/*; do
     sed -i "s?__CALLBACK__?$HEROKU_URL:$PORT\/callback?" $filename
 done
 
+node herokuAuth0Update.js
+
 yarn serve-web
