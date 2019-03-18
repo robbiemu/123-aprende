d11839e5463a3340395253d43117e7d21bb26adf
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-17 22:17:12 -0400

    revised sed

--- herokuInit.sh
+++ herokuInit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for filename in build/static/js; do
-    sed -i "s/__CALLBACK__/$HEROKU_URL:$PORT\/callback/" $filename
+for filename in build/static/js/*; do
+    sed -i "s?__CALLBACK__?$HEROKU_URL:$PORT\/callback?" $filename
 done
 
 yarn serve-web
