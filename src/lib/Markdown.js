846d6641777690a774071de0a62e9d8cd6a404e5
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-24 19:04:14 -0400

    external links

--- src/lib/Markdown.js
+++ src/lib/Markdown.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text } from 'react-native-paper'
-import {getUniqueID} from 'react-native-markdown-renderer'
+import { getUniqueID, openUrl } from 'react-native-markdown-renderer'
 
 import history from '@src/lib/history'
 
@@ -8,11 +8,18 @@ export let rules = {
   link: (node, children, parent, styles) => {
     let destination = (node && node.attributes) ? node.attributes.href: undefined
     if(destination) {
-      destination = destination.replace(/^(local|vocabulary):\/\//, '/')
+      if(/^(?:local|vocabulary):\/\//.test(destination)) {
+        destination = destination.replace(/^(local|vocabulary):\/\//, '/')
 
-      return <Text key={ getUniqueID() } mode="text" onPress={() => history.push(destination)}>
-        [{ children }]
-      </Text>
+        return <Text key={ getUniqueID() } mode="text" onPress={() => history.push(destination)}>
+          [{ children }]
+        </Text>
+      } else {
+        // this is the original link renderer for markdown: https://github.com/mientjan/react-native-markdown-renderer/blob/master/src/lib/renderRules.js
+        return <Text key={ getUniqueID() } style={styles.link} onPress={() => openUrl(node.attributes.href)}>
+          {children}
+        </Text>
+      }
     } else {
       return <Text key={ getUniqueID() } mode="text">
         { children }
