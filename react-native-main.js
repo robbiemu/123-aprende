cc23e2ebe5bb8ea1c97b4d1d5997e2b70c20b379
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2019-03-28 22:36:58 -0400

    safer start

--- react-native-main.js
+++ react-native-main.js
@@ -2,8 +2,12 @@ import { KeepAwake, registerRootComponent } from 'expo'
 import App from './src/Controllers/App'
 
 import AsyncStorage from '@callstack/async-storage'
-AsyncStorage.clear()
-
+try {
+  AsyncStorage.clear()
+} catch (e) {
+  // no error
+}
+debugger
 if (__DEV__) {
   KeepAwake.activate()
 }
